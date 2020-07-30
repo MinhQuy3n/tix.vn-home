637f5796de9fda8c6d7378b351e78726331172b1
Minh Quyền <MQuyennn@github.com>
2020-07-30 18:07:50 +0700

    Update

--- js/JS/carousel.js
+++ js/JS/carousel.js
@@ -1,33 +1,28 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-      loop: true,
-      margin: 10,
-      nav: true,
-      dots:false,
-      responsive : {
-        // breakpoint from 0 up
-        0 : {
-            items : 1,
-          
-        },
-        // breakpoint from 480 up
-        480 : {
-          items : 2,
-
-            
-        },
-        // breakpoint from 768 up
-        768 : {
-          items : 4, 
-        },
-        992: {
-          items: 4,
-        },
-        1200:{
-          items: 1,
-        }
-    }
-    
-    });
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 1,
+      },
+      // breakpoint from 480 up
+      480: {
+        items: 2,
+      },
+      // breakpoint from 768 up
+      768: {
+        items: 1,
+      },
+      992: {
+        items: 1,
+      },
+      1200: {
+        items: 1,
+      },
+    },
   });
-  
+});
